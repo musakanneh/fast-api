9ad2b7e07ceee6749c03af616c12d56bbb4c6234
Musa Selekie Kanneh <musas.kanneh@gmail.com>
2022-04-03 16:08:49 +0100

    v1 enpoint added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# fast-api
+## Fast API in Action
+
+### Userful Readings
+- [Concurrency and async / await](https://fastapi.tiangolo.com/async/)
+- 
+
--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from fastapi import FastAPI
+from typing import List
+from models import User, Gender, Role
+from uuid import UUID, uuid4
 
 app = FastAPI()
+
+db:List[User] = [
+    User(
+        id=uuid4(),
+        first_name="Musa",
+        last_name="Kanneh",
+        gender=Gender.male,
+        roles=[Role.admin]
+    ),
+     User(
+        id=uuid4(),
+        first_name="Fatu",
+        last_name="Kanneh",
+        gender=Gender.female,
+        roles=[Role.admin, Role.user]
+    )
+]
 
 @app.get("/")
 def root():
     return {"Hello": "World"}
 
-@app.get("/items/{id}")
-def read_item(id:int, ):
-    return {"id": id}
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db;
